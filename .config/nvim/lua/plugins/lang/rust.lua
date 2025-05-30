c34dc1cb01fd8d0e83256c4d6fd7531f49e33f0a
quike <616137+quike@users.noerply.github.com>
2025-05-30 14:30:08 -0400

    fix: correct rust tools lang plugin

--- .config/nvim/lua/plugins/lang/rust.lua
+++ .config/nvim/lua/plugins/lang/rust.lua
@@ -3,6 +3,6 @@ return {
   {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
-    lazy = false,   -- This plugin is already lazy
+    lazy = true,    -- This plugin is already lazy
   }
 }
