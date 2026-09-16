9d9c3f24f6ade0b90f9db57e98ca65cff1af574d
quike <616137+quike@users.noreply.github.com>
2026-09-16 09:27:54 -0400

    style(nvim): apply stylua formatting

--- .config/nvim/lua/plugins/lang/rust.lua
+++ .config/nvim/lua/plugins/lang/rust.lua
@@ -1,8 +1,8 @@
 return {
   -- overwrite Rust tools inlay hints
   {
-    'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
-    lazy = true,    -- This plugin is already lazy
-  }
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = true, -- This plugin is already lazy
+  },
 }
